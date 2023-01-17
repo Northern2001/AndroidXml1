daaeae4e6df44335031c5e52e1a74e4113e89e40
Northern2001 <122859519+Northern2001@users.noreply.github.com>
2023-01-17 13:51:14 +0700

    add project

---
+++ LearnXML1/app/src/main/java/com/example/learnxml1/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.learnxml1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    val binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+
+
+    }
+
+}
